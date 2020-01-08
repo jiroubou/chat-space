936c9956735ac44f9b3d42427ac14cec84df5f5a
jiroubou <59195911+jiroubou@users.noreply.github.com>
2020-01-08 20:05:18 +0900

    chat

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,7 +6,30 @@ class GroupsController < ApplicationController
   end
 
   def create
-    binding.pry
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [] )
   end
 
 end
