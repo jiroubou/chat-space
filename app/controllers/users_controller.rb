1b41711d99eafebe3df3ed1523a48d0342b7362c
jiroubou <59195911+jiroubou@users.noreply.github.com>
2020-01-15 13:12:31 +0900

    chat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   def edit
   end
+      
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 
   def update
     if current_user.update(user_params)
